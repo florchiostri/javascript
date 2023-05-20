f149495eda602fc573c4456eabd7ab71a46b8d1a
Flor Chiostri <130665833+florchiostri@users.noreply.github.com>
2023-05-20 19:10:28 -0300

    flora.js
    ejercicio

---
+++ flora.js
@@ -0,0 +1,14 @@
+var nombre= "Florencia"; //string
+var apellido= "Chiostri"; //string
+
+console.log(nombre+ " "+apellido);
+//resultado= Florencia Chiostri
+
+var number= 3; //number1
+var numero= 2; //number2
+var suma= number+numero; //suma resultado=5
+console.log(suma);
+
+var tres= suma+1; //number3
+console.log(tres); //resultado 6
+
